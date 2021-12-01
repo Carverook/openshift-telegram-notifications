46da82be52bc241e6a139ea3a2bb4a0ebd6e8aae
Carverok <alamabil15@gmail.com>
2021-12-01 19:49:49 +0200

    req

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# openshift-telegram-notifications
+# OpenShift Telegram Notifications
 
+A project to send OpenShift error messages to a telegram channel/group of your choice.
+
+## Cluster Deployment
+
+First create a [Telegram Bot](https://core.telegram.org/bots#6-botfather).
+
+Then deploy this bot to OpenShift with permissions via:
+
+```shell
+$ oc adm policy add-cluster-role-to-user cluster-reader system:serviceaccount:<current-project-here>:default --as=system:admin
+$ oc new-app -f https://raw.githubusercontent.com/dinhnn/openshift-telegram-notifications/master/template.yaml \
+             -p TELEGRAM_API_URL=https://api.telegram.org \
+             -p TELEGRAM_BOT_TOKEN=<bot-token> \
+             -p TELEGRAM_BOT_CHANNEL=<chat_id of channel or group> \
+             -p OPENSHIFT_CONSOLE_URL=https://<openshift-host-here>:8443/console
+$ oc start-build openshift-telegram-notifications
+```
+
+Once the app is built and deployed, it will start sending notifications to slack when there are `Warning` type events.
+
+![Telegram Message](images/telegram-bot-message.png)
+
+## Local Development
+
+### Cluster Requirements
+
+First you need a running minishift cluster. This can be installed via homebrew:
+
+```shell
+$ brew install socat openshift-cli docker-machine-driver-xhyve
+$ brew tap caskroom/versions
+$ brew cask install minishift-beta
+```
+
+The xhyve hypervisor requires superuser privileges. To enable, execute:
+
+```shell
+$ sudo chown root:wheel /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+$ sudo chmod u+s /usr/local/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+```
+
+Then start the cluster with:
+
+```shell
+$ minishift start --memory 4048
+```
+
+### App Requirements
+
+First add the privileges to mount volumes and read cluster state to your service account:
+
+```shell
+$ oc login -u system:admin
+$ oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:myproject:default --as=system:admin
+$ oc adm policy add-cluster-role-to-user cluster-reader system:serviceaccount:myproject:default --as=system:admin
+```
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"net/url"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/rest"
+	"strings"
+	"time"
+)
+
+
+func resourceUrl(event *v1.Event) string {
+	return os.Getenv("OPENSHIFT_CONSOLE_URL") + "/project/" + event.InvolvedObject.Namespace + "/browse/" + strings.ToLower(event.InvolvedObject.Kind) + "s/" + event.InvolvedObject.Name
+}
+
+func monitoringUrl(event *v1.Event) string {
+	return os.Getenv("OPENSHIFT_CONSOLE_URL") + "/project/" + event.InvolvedObject.Namespace + "/monitoring"
+}
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
+func notifyTelegram(event *v1.Event) {
+	telegramApiUrl := getEnv("TELEGRAM_API_URL","https://api.telegram.org")
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	channelName := os.Getenv("TELEGRAM_CHANNEL_"+strings.ToUpper(strings.ReplaceAll(event.InvolvedObject.Namespace,"-","_")))
+	if len(channelName) == 0 {
+		channelName = os.Getenv("TELEGRAM_CHANNEL")
+	}
+	message := "Project: "+event.InvolvedObject.Namespace+" "+monitoringUrl(event)+"\n" +
+	event.InvolvedObject.Kind + ": " + event.InvolvedObject.Name+" "+ resourceUrl(event)+"\n"+
+		event.Message+"\nReason: " + event.Reason
+	params := url.Values{}
+	params.Add("chat_id",channelName)
+	params.Add("text", message)
+
+	client := http.Client{}
+	
+	req, err := http.NewRequest("POST", telegramApiUrl+"/bot"+botToken+"/sendMessage", bytes.NewBufferString(params.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	_, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Unable to reach the server.")
+	}
+}
+
+func watchEvents(clientset *kubernetes.Clientset) {
+	startTime := time.Now()
+	log.Printf("Watching events after %v", startTime)
+
+	watcher, err := clientset.CoreV1().Events("").Watch(v1.ListOptions{FieldSelector: "type=Warning"})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for watchEvent := range watcher.ResultChan() {
+		event := watchEvent.Object.(*v1.Event)
+		if event.FirstTimestamp.Time.After(startTime) {
+			notifyTelegram(event)
+		}
+	}
+}
+
+func main() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	go func() {
+		for {
+			watchEvents(clientset)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	log.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
